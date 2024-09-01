d0f6df90025c49ebaa8187127ffc92e6c10cdcef
Bayronix <151779642+Bayronix@users.noreply.github.com>
2024-09-01 19:01:43 +0200

    comm

--- src/server.js
+++ src/server.js
@@ -1,38 +1,48 @@
 import express from 'express';
-
 import cors from 'cors';
+import pino from 'pino-http';
+import { env } from './utils/dovenv.js';
 
-const PORT = 3001;
+const PORT = Number(env('PORT', 3001));
 
-const setupServer = express();
+export const startServer = () => {
+  const setupServer = express();
 
-setupServer.use(cors());
+  setupServer.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-setupServer.use(express.json());
+  setupServer.use(cors());
+  setupServer.use(express.json());
 
-setupServer.get('/', (req, res) => {
-  req.log.info('Root route accessed');
-  res.json({
-    message: `Server is running on port ${PORT}`,
+  setupServer.get('/', (req, res) => {
+    req.log.info('Root route accessed');
+    res.json({
+      message: `Server is running on port ${PORT}`,
+    });
   });
-});
 
-setupServer.listen(PORT, () => {
-  {
-    console.log(`Server started ${PORT}`);
-  }
-});
-
-setupServer.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Not found',
+  setupServer.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
   });
-});
 
-setupServer.use((err, req, res, next) => {
-  req.log.error(err, 'Something went wrong');
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
+  setupServer.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
   });
-});
+
+  setupServer.use((err, req, res, next) => {
+    req.log.error(err, 'Something went wrong');
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+};
+//
+startServer();
